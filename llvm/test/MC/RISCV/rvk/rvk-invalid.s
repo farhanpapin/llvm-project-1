a13db0926293358a6684ed54c13796f117135049
qixingxue <qixingxue@outlook.com>
2021-03-04 15:16:25 +0800

    Replace register a0 with t0 to match the spec

--- llvm/test/MC/RISCV/rvk/rvk-invalid.s
+++ llvm/test/MC/RISCV/rvk/rvk-invalid.s
@@ -1,7 +1,6 @@
 # RUN: not llvm-mc -triple riscv32 -mattr=+experimental-k < %s 2>&1 \
 # RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
 
-<<<<<<< HEAD
 sm4ed t0, a2, 8
 # CHECK-ERROR: immediate must be an integer in the range [0, 3]
 
